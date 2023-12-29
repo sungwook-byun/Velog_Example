8bdea0290a93bf13bbbba2fea8acb0030fd76d4d
Sungwook <qustjddnr11@naver.com>
2023-12-29 21:26:36 +0900

    study

--- Desktop/Study/51th_c++/51th_c++/List.cpp
+++ Desktop/Study/51th_c++/51th_c++/List.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+#include <vector>
+using std::vector;
+
+#include <list>
+using std::list;
+
 #include "List.h"
 
 struct CharInfo
@@ -40,6 +46,7 @@ public:
 };
 
 List<CharInfo> charlist;
+list<CharInfo> stdcharlist;
 
 int main()
 {
@@ -47,6 +54,64 @@ int main()
 	charlist.push_back(CharInfo(L"Warrior", 100, 30, 50));
 	charlist.push_back(CharInfo(L"Archer", 80, 50, 40));
 	charlist.push_back(CharInfo(L"Wizzard", 50, 100, 20));
+
+	stdcharlist.push_back(CharInfo(L"Warrior", 100, 30, 50));
+	stdcharlist.push_back(CharInfo(L"Archer", 80, 50, 40));
+	stdcharlist.push_back(CharInfo(L"Wizzard", 50, 100, 20));
+
+	list<CharInfo>::iterator listiter;
+
+	listiter = stdcharlist.begin();
+	--listiter;
+
+	for (listiter = stdcharlist.begin(); listiter != stdcharlist.end(); ++listiter)
+	{
+		std::wcout << (*listiter).szName << std::endl;
+	}
+
+	listiter = stdcharlist.begin();
+	++listiter;
+
+	// list 의 insert 함수 테스트
+	stdcharlist.insert(listiter, CharInfo(L"Berserker", 80, 10, 80));
+	const CharInfo& info = (*listiter);
+
+	// list 의 erase 함수 테스트
+	// Warrior - Berserker - (Archer) - Wizzard
+	listiter = stdcharlist.begin();
+	++listiter;
+	++listiter;
+
+	listiter = stdcharlist.erase(listiter);
+	const CharInfo& charinfo = *listiter;
+
+
+	// erase 문제
+	vector<int> vecInt;
+	for (int i = 0; i < 100; ++i)
+	{
+		// 1 ~ 100 까지 입력
+		vecInt.push_back(i + 1);
+	}
+
+	// erase 사용하는 경우 iterator 가 문제가 되지 않게, 삭제된 다음 iterator 를 돌려받는 구조
+	// 따라서 반복문에서 iterator 를 매번 ++ 하는 경우 erase 함수와 중복되어서 
+	// 가리키는 요소를 두번 증가하게 될 수 있다.
+	vector<int>::iterator veciter = vecInt.begin();
+	for (; veciter != vecInt.end(); )
+	{
+		if ((*veciter) <= 50)
+		{
+			veciter = vecInt.erase(veciter);
+		}
+		else
+		{
+			++veciter;
+		}
+	}
+
+
+
 
 	return 0;
 }
--- Desktop/Study/51th_c++/51th_c++/List.h
+++ Desktop/Study/51th_c++/51th_c++/List.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <assert.h>
 
 template<typename T>
 struct Node
@@ -31,6 +32,20 @@ private:
 
 public:
 	void push_back(const T& _Data);
+	void push_front(const T& _Data);
+
+
+	class iterator;
+
+	iterator begin() { return iterator(this, m_pHead); }
+	iterator end() { return iterator(this, nullptr); }
+
+
+	// _targetIter 가 가리키는곳에 _Data 를 저장시키고, 새로 저장한 데이터를 가리키는 iterator 를 반환해준다.
+	// iterator insert(iterator _targetIter, const T& _Data);
+
+	// _targetIter 가 가리키는 데이터를 삭제하고, 삭제한 다음을 가리키는 iterator 를 반환
+	// iterator erase(iterator _targetIter);
 
 
 public:
@@ -51,6 +66,67 @@ public:
 			pNode = pNext;
 		}
 	}
+
+public:
+	class iterator
+	{
+	private:
+		List<T>* m_Owner;
+		Node<T>* m_TargetNode;
+
+	public:
+		T& operator *()
+		{
+			// Owner(List) 가 설정되어있지 않으면, 정상적인 iterator 가 아니다.
+			// Owner 가 설정되어 있어도, m_TargetNode 가 nullptr 이면 End Iterator 이기 때문에
+			// 마지막의 다음을 가리키는 상태의 iterator 에게 * 로 접근기능을 쓰면 에러
+			assert(m_Owner && m_TargetNode);
+			return m_TargetNode->Data;
+		}
+
+		// 1. ++, -- 반환타입 문제
+		// 2. ++, -- 후위연산자 문제
+		void operator ++()
+		{
+			// enditerator 에서 ++ 하는 경우
+			assert(m_Owner && m_TargetNode);
+			m_TargetNode = m_TargetNode->pNext;
+		}
+
+		void operator --()
+		{
+			assert(m_Owner && m_TargetNode);
+			assert(m_Owner->m_pHead != m_TargetNode); // iterator 가 begin 이다.			
+			m_TargetNode = m_TargetNode->pPrev;
+		}
+
+		bool operator == (const iterator& _otheriter)
+		{
+			if (m_Owner == _otheriter.m_Owner && m_TargetNode == _otheriter.m_TargetNode)
+				return true;
+			else
+				return false;
+		}
+
+		bool operator != (const iterator& _otheriter)
+		{
+			return !((*this) == _otheriter);
+		}
+
+	public:
+		iterator()
+			: m_Owner(nullptr)
+			, m_TargetNode(nullptr)
+		{}
+
+		iterator(List<T>* _Owner, Node<T>* _TargetNode)
+			: m_Owner(_Owner)
+			, m_TargetNode(_TargetNode)
+		{}
+
+		~iterator()
+		{}
+	};
 };
 
 template<typename T>
@@ -70,5 +146,23 @@ void List<T>::push_back(const T& _Data)
 		m_pTail = pNewNode;
 	}
 
+	++m_CurCount;
+}
+
+template<typename T>
+inline void List<T>::push_front(const T& _Data)
+{
+	Node<T>* pNewNode = new Node<T>(_Data, m_pHead, nullptr);
+
+	if (nullptr != m_pHead)
+	{
+		m_pHead->pPrev = pNewNode;
+	}
+	else
+	{
+		m_pTail = pNewNode;
+	}
+
+	m_pHead = pNewNode;
 	++m_CurCount;
 }
